661ef746f0fc17916a2174db98181fc04a37e8cc
bwong <bwong@sfs-wifi-dhcp-10-143-214-131.wifi.sfsu.edu>
2016-12-18 19:40:57 -0800

    Initial Commit

---
+++ Tip Caculator/ViewController.swift
@@ -0,0 +1,51 @@
+//
+//  ViewController.swift
+//  Tip Caculator
+//
+//  Created by mwong on 12/17/16.
+//  Copyright © 2016 mwong. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var billField: UITextField!
+    @IBOutlet weak var tipLabel: UILabel!
+    @IBOutlet weak var totalLabel: UILabel!
+    @IBOutlet weak var twoPersonLabel: UILabel!
+    @IBOutlet weak var threePersonLabel: UILabel!
+    @IBOutlet weak var tipControl: UISegmentedControl!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    @IBAction func calcTip(sender: AnyObject) {
+        
+        let tipPercentages = [0.15, 0.2, 0.25]
+        
+        let bill = Double(billField.text!) ?? 0
+        let tip = bill * tipPercentages[tipControl.selectedSegmentIndex]
+        
+        let total = bill + tip
+        
+        tipLabel.text = String(format:"$%.2f", tip)
+        totalLabel.text = String(format:"$%.2f", total)
+        
+        twoPersonLabel.text = String(format: "$%.2f", total / 2)
+        threePersonLabel.text = String(format: "$%.2f", total / 3)
+        
+    }
+    
+    @IBAction func onTap(sender: AnyObject) {
+        view.endEditing(true)
+    }
+}
+
